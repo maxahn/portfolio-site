701f295e530a1ed15fd8edffac673b5a3cfff63a
Max Ahn <ahnmaxwell@gmail.com>
2025-05-01 17:33:51 -0600

    - update favicon ico

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -28,8 +28,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Maxwell Ahn',
+  description: "Maxwell's Portfolio website",
 };
 
 /**
